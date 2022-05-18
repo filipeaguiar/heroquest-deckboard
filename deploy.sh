254d3a867f75e997e8461f6940881b983ec11880
Filipe Aguiar <filipe_aac@yahoo.com.br>
2022-05-18 13:47:45 -0300

    Deploy e nova build

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:filipeaguiar/heroquest-deckboard.git main:gh-pages
+
+cd -
