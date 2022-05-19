508e985bb532574295bbaccbf87a134356689587
Filipe Aguiar <filipe_aac@yahoo.com.br>
2022-05-19 11:13:00 -0300

    alterado deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,3 @@
 #!/usr/bin/sh
 
-# abort on errors
-set -e
-
-# build
-npm run build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git checkout -b master
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:filipeaguiar/heroquest-deckboard.git main:gh-pages
-
-cd -
+git subtree push --prefix dist master gh-pages
